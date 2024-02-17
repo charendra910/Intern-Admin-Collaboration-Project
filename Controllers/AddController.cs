ea361f3ac833aaa62990639a0190e94e3ebc498c
charendra910 <charendra910@rku.ac.in>
2024-02-17 18:56:18 +0530

    add intern updated

--- Controllers/AddController.cs
+++ Controllers/AddController.cs
@@ -6,10 +6,22 @@ namespace Intern_Admin_Collaboration.Controllers
 {
     public class AddController : Controller
     {
-       
+        private Addcontext s1;
+
+        public AddController(Addcontext s1)
+        {
+            this.s1 = s1;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Addintern()
+        {
+            var addintern = s1.AddIntern.ToList();
+            return View(addintern);
         }
     }
 }
