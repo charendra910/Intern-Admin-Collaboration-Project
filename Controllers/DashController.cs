ade4cb03fd4631649b446c0adafd985deebe9ddb
charendra910 <charendra910@rku.ac.in>
2024-02-20 23:31:55 +0530

    admin dash done

---
+++ Controllers/DashController.cs
@@ -0,0 +1,26 @@
+﻿using Intern_Admin_Collaboration.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intern_Admin_Collaboration.Controllers
+{
+    public class DashController : Controller
+    {
+        public Dashcontext s2;
+
+        public DashController(Dashcontext s2)
+        {
+            this.s2 = s2;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Admindash()
+        {
+            var admins = s2.DashboardIntern.ToList();
+            return View(admins);
+        }
+    }
+}
