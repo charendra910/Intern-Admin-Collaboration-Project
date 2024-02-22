92bc19beb60242282c51600df92be5a39a3fa213
charendra910 <charendra910@rku.ac.in>
2024-02-22 11:38:00 +0530

    update intern

--- Controllers/AddController.cs
+++ Controllers/AddController.cs
@@ -1,6 +1,7 @@
 ﻿using Intern_Admin_Collaboration.Data;
 using Intern_Admin_Collaboration.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Intern_Admin_Collaboration.Controllers
 {
@@ -18,12 +19,26 @@ namespace Intern_Admin_Collaboration.Controllers
             return View();
         }
 
-        public IActionResult Showinterns ()
-        {
-            var addintern = s1.AddIntern.ToList();
-            return View(addintern);
+        //public IActionResult Showinterns ()
+        //{
+        //    var addintern = s1.AddIntern.ToList();
+        //    return View(addintern);
 
+        //}
+
+        public async Task<IActionResult> ShowInterns(string searchString)
+        {
+            var interns = await s1.AddIntern.ToListAsync();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                interns = interns.Where(s1 => s1.firstname.Contains(searchString) || s1.lastname.Contains(searchString)).ToList();
+            }
+
+            return View(interns);
         }
+
+
         public IActionResult Addintern()
         {
             var addintern = s1.AddIntern.ToList();
